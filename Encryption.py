edca6e0e4bdf869865323797b82b4fb2000b7cf9
Mohamad Abbas <its.hamodi2005@gmail.com>
2025-10-05 01:57:58 +0200

    load and save (.json)

--- Encryption.py
+++ Encryption.py
@@ -1,5 +1,13 @@
 import random
 import string
+import json
+
+all_chars = string.ascii_letters + string.digits + string.punctuation
+all_chars = list(all_chars)
+
+key = all_chars.copy()
+random.shuffle(key)
+saved_chiper =""
 
 def Encryption():
     print("----Encryption ----")
@@ -25,19 +33,29 @@ def Decryption():
     print(f"Your text is: {text}")
     return text
 
-all_chars = string.ascii_letters + string.digits + string.punctuation
-all_chars = list(all_chars)
+def save_key(filename, key, all_chars):
 
-key = all_chars.copy()
-random.shuffle(key)
-saved_chiper =""
+    data = {"key": key, "all_chars":all_chars}
+    with open(filename,"w") as file:
+        json.dump(data, file)
+
+def load_key(filename):
+
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+    key = data["key"]
+    all_chars = data["all_chars"]
+    print(f"Key loaded from {filename}")
+    return key, all_chars
 
 
-print("*" * 50)
 
 while True:
     try:
-        print("\n(1) Encryption (2) Decryption (3) Save this key (4) Exit")
+        print("\n\n")
+        print("*" * 100)
+        print("\n\n(1) Encryption (2) Decryption (3) Save this key (4) Loud a key file (5) Exit")
         userchoice = (input("\nHow can i help...: "))
     except ValueError:
         print("You need to choose between 1 and 2 ")
@@ -50,16 +68,22 @@ while True:
 
     elif userchoice == "3":
         filename = input("Give your key file a name: ")
-        with open(filename, "w") as file:
-            try:
-                file.write(str(key)+ "\n\n")
-                file.write(str(all_chars)+ "\n\n")
-                file.write(str(saved_chiper)+ "\n\n")
-            except NameError:
-                print("")
+        if not filename.endswith(".json"):
+            filename += ".json"
+        save_key(filename, key, all_chars)
 
     elif userchoice == "4":
+        filename = input("Filename to load key from: ")
+        if not filename.endswith(".json"):
+            filename += ".json"
+
+        try:
+            key, all_chars = load_key(filename)
+        except FileNotFoundError:
+            print(f"\nThere is no saved file with the name {filename} please try agian. ")
+    
+    elif userchoice == "5":
         break
 
     else:
-        print("You need to choose between 1 and 4 ")
+        print("You need to choose between 1 and 5 ")
